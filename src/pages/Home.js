505236cad1d13530d6caf79854ab8b19e37396f3
FreekJanssen <freekwjanssen@gmail.com>
2020-06-16 13:18:31 +0200

    Add dummy Homepage + frontend route

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import menu from '../../src/menu_simplified.png'
+
+export default function Home() {
+  return (
+    <div>
+      <img src={menu}/>     
+    </div>
+  );
+};
